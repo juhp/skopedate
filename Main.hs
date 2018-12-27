c268bbf906758c6f6038f90b60be19978740eec4
Jens Petersen <petersen@redhat.com>
2018-12-27 12:31:07 +0900

    use microlens-aeson to get Created time

--- Main.hs
+++ Main.hs
@@ -1,36 +1,18 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
 import SimpleCmd
-import Data.Aeson
-import Data.Char (toUpper)
+-- import Data.Aeson
 -- import Data.Text (Text)
---import qualified Data.Text.IO as T
-import GHC.Generics
+import qualified Data.Text as T
 import System.Environment (getArgs)
-import Data.Time.Clock (UTCTime)
-import Data.Time.LocalTime (ZonedTime, utcToLocalZonedTime, zonedTimeToUTC)
+--import Data.Time.Clock (UTCTime)
+--import Data.Time.LocalTime (ZonedTime, utcToLocalZonedTime, zonedTimeToUTC)
 
-data Image =
-  Image { -- name  :: !Text
---        , digest :: !Text
---        , repoTags :: [Text],
-          created :: !ZonedTime
---        , dockerVersion :: !Text
---        , labels :: !Object
---        , architecture :: !Text
---        , os :: !Text
---        , layers :: [Text]
-        } deriving (Show, Generic)
+import Lens.Micro
+import Lens.Micro.Aeson
 
-customOptions :: Options
-customOptions = defaultOptions
-                { fieldLabelModifier = \ (c:cs) -> toUpper c : cs
-                }
-
-instance FromJSON Image where
-    parseJSON = genericParseJSON customOptions
 
 main :: IO ()
 main = do
@@ -38,18 +20,14 @@ main = do
   let image = head args
   t1 <- checkImage "docker.io" image
   t2 <- checkImage "registry.fedoraproject.org" image
-  putStrLn $ "docker.io" +-+ (show $ compare t1 t2) +-+ "fedoraproject"
-    
+  putStrLn $ "docker.io" +-+ show (compare t1 t2) +-+ "fedoraproject"
 
-checkImage :: String -> String -> IO UTCTime
+checkImage :: String -> String -> IO String
 checkImage registry image = do
   let ref = registry ++ "/" ++ image
-  out <- cmd "skopeo" ["inspect", "docker://" ++ ref]
-  let res = eitherDecode out
-  case res of
-    Left err -> error err
-    Right img -> do
-      let utc = zonedTimeToUTC $ created img
-      lzt <- utcToLocalZonedTime utc
-      putStrLn $ show lzt +-+ ref
-      return utc
+  out <- cmd "skopeo" ["inspect", "docker://" ++ ref] :: IO String
+  let created = out ^. key "Created" . _String & T.unpack
+--      utc = zonedTimeToUTC created
+--  lzt <- utcToLocalZonedTime utc
+  putStrLn $ created {- $ show lzt -} +-+ registry
+  return created -- utc
