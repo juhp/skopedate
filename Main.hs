5cf4c7704914125b0314f9cfa04b1392e6f48ea8
Jens Petersen <petersen@redhat.com>
2019-03-07 15:42:06 +0800

    compare and sort docker, fedora and fedora candidate registeries

--- Main.hs
+++ Main.hs
@@ -4,11 +4,13 @@ module Main (main) where
 
 import SimpleCmd
 -- import Data.Aeson
--- import Data.Text (Text)
+import Data.List (sortBy)
+import Data.Time.Clock (UTCTime)
+import Data.Time.LocalTime (utcToLocalZonedTime)
+import Data.Time.Format (defaultTimeLocale, iso8601DateFormat, parseTimeOrError)
+import Data.Text (Text)
 import qualified Data.Text as T
 import System.Environment (getArgs)
---import Data.Time.Clock (UTCTime)
---import Data.Time.LocalTime (ZonedTime, utcToLocalZonedTime, zonedTimeToUTC)
 
 import Lens.Micro
 import Lens.Micro.Aeson
@@ -18,16 +20,33 @@ main :: IO ()
 main = do
   args <- getArgs
   let image = head args
-  t1 <- checkImage "docker.io" image
-  t2 <- checkImage "registry.fedoraproject.org" image
-  putStrLn $ "docker.io" +-+ show (compare t1 t2) +-+ "fedoraproject"
+  checkRegistries ["docker.io","registry.fedoraproject.org","candidate-registry.fedoraproject.org"] image
+--  putStrLn $ show "docker.io" +-+ show (compare t1 t2) +-+ "fedoraproject"
 
-checkImage :: String -> String -> IO String
-checkImage registry image = do
-  let ref = registry ++ "/" ++ image
-  out <- cmd "skopeo" ["inspect", "docker://" ++ ref] :: IO String
-  let created = out ^. key "Created" . _String & T.unpack
+checkRegistries :: [String] -> String -> IO ()
+checkRegistries rs image = do
+  let refs = map (\ r -> "docker://" ++ r ++ "/" ++ image) rs
+  times <- mapM skopeoInspectTime refs
 --      utc = zonedTimeToUTC created
 --  lzt <- utcToLocalZonedTime utc
-  putStrLn $ created {- $ show lzt -} +-+ registry
-  return created -- utc
+  mapM_ printTime $ sortBy timeOrder $ zip times rs
+--  return created -- utc
+  where
+    timeOrder (t,_) (t',_) = compare t' t
+
+    printTime (u,s) = do
+      t <- utcToLocalZonedTime u
+      putStrLn $ show t ++ " " ++ s
+
+skopeoInspectTime :: String -> IO UTCTime
+skopeoInspectTime ref = do
+  out <- cmd "skopeo" ["inspect", ref] :: IO Text
+  let created = out ^. key "Created" . _String & T.unpack & removeSplitSecs
+  return $ parseTimeOrError False defaultTimeLocale (iso8601DateFormat (Just "%H:%M:%SZ")) created
+  where
+    -- docker.io has nanosec!
+    removeSplitSecs :: String -> String
+    removeSplitSecs cs =
+      case break (== '.') cs of
+        (cs', "") -> cs'
+        (cs', _) -> cs' ++ "Z"
